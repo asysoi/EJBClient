9b485a27fd00c33263b9c08dd4c1ce0e6cdef876
asysoi <asysoi@cci.by>
2019-04-16 16:48:51 +0300

    Added EJB annotation

--- EJBClient/appClientModule/Main.java
+++ EJBClient/appClientModule/Main.java
@@ -1,16 +1,15 @@
 import java.util.Properties;
 
 import cci.cert.IOwnService;
+
+import javax.ejb.EJB;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.net.Socket;
-import java.util.Scanner;
 
 public class Main {
+	@EJB(beanName="cci.cert.IOwnService")
+	IOwnService service;
+	
 	public static void main(String[] args) {
 		Main x = new Main();
 		x.connect();
@@ -19,17 +18,16 @@ public class Main {
 	public Main() {
 		super();
 	}
-
+	
 	private void connect() {
 		try {
 			Properties props = new Properties();
 			props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
 			props.setProperty("org.omg.CORBA.ORBInitialHost", "192.168.0.72");
 			props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
-
 			InitialContext ctx = new InitialContext(props);
-			System.out.println(ctx.getEnvironment());
-			IOwnService service = (IOwnService) ctx.lookup("cci.cert.IOwnService");
+			
+			service = (IOwnService) ctx.lookup("cci.cert.IOwnService");
 			System.out.println(service.getType());
 
 		} catch (Exception ex) {
